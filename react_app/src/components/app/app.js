5bf5f373c53d8d6db70dd214b9ddeb81eb8ea59e
Kraine Kraine <kraine212@gmail.com>
2024-12-24 15:21:16 +0200

    1

---
+++ react_app/src/components/app/app.js
@@ -0,0 +1,25 @@
+
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersListItem from '../employers-list-item/employers-list-item';
+// import './app.css';
+
+
+function App() {
+    return (
+        <div className="app">
+            <AppInfo/>
+            
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+            <EmployersListItem></EmployersListItem>
+
+        </div>
+    );
+}
+
+
+export default App;
